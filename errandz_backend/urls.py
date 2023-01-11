05d7c8f7e810ed231bbf52f21992e78a4191e963
winning godspower <winninggodspower@gmail.com>
2023-01-11 01:36:02 +0100

    fixed static file in production issues

--- errandz_backend/urls.py
+++ errandz_backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/', include('user_auth.urls')),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
