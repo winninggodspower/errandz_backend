5f53e9556855a90be742c8d8fe7a88e18a2269aa
winning godspower <winninggodspower@gmail.com>
2023-01-29 12:04:02 +0100

    added user image to endpoint

--- user_auth/serializers.py
+++ user_auth/serializers.py
@@ -2,30 +2,35 @@ from rest_framework import serializers
 from user_auth.models import Account, Rider, Customer, Vendor
 from phonenumber_field.serializerfields import PhoneNumberField
 
+
 class AccountRegistrationSerializer(serializers.ModelSerializer):
     phone = PhoneNumberField(region="NG")
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True)
+
     class Meta:
-      model = Account
-      fields = ['id', 'email', 'phone', 'state', 'city', 'password', 'password2', 'user_type']
-      extra_kwargs = {
-        'password': {'write_only': True},
-        'user_type': {'required': False}
-      }
+        model = Account
+        fields = ['id', 'email', 'phone', 'state',
+                  'city', 'password', 'password2', 'user_type', 'profile_image']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'user_type': {'required': False}
+        }
 
     def save(self):
         account = Account(
-          email = self.validated_data['email'],
-          phone = self.validated_data['phone'],
-          state = self.validated_data['state'],
-          city = self.validated_data['city'],
-          user_type = self.validated_data['user_type']
+            email=self.validated_data['email'],
+            phone=self.validated_data['phone'],
+            state=self.validated_data['state'],
+            city=self.validated_data['city'],
+            user_type=self.validated_data['user_type']
         )
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
 
         if password != password2:
-          raise serializers.ValidationError({'password2': 'Passwords must match'})
+            raise serializers.ValidationError(
+                {'password2': 'Passwords must match'})
 
         account.set_password(password)
         account.save()
@@ -33,12 +38,13 @@ class AccountRegistrationSerializer(serializers.ModelSerializer):
 
 
 class RiderRegisterSerializer(serializers.ModelSerializer):
-      account = AccountRegistrationSerializer()
-      class Meta:
+    account = AccountRegistrationSerializer()
+
+    class Meta:
         model = Rider
         fields = ['first_name', 'last_name', 'account']
 
-      def create(self, validated_data):
+    def create(self, validated_data):
         account_data = validated_data.pop('account')
         account_data['user_type'] = 'rider'
         print(account_data)
@@ -48,50 +54,56 @@ class RiderRegisterSerializer(serializers.ModelSerializer):
         rider = Rider.objects.create(**validated_data, account=account)
         return rider
 
+
 class CustomerRegisterSerializer(serializers.ModelSerializer):
-  account = AccountRegistrationSerializer()
-  class Meta:
-    model = Customer
-    fields = ['first_name', 'last_name', 'account']
+    account = AccountRegistrationSerializer()
 
-  def create(self, validated_data):
-    account_data = validated_data.pop('account')
-    account_data['user_type'] = 'customer'
-    print(account_data)
-    account = AccountRegistrationSerializer(data=account_data)
-    account.is_valid()
-    account = account.save()
-    customer = Customer.objects.create(**validated_data, account=account)
-    return customer
-  
+    class Meta:
+        model = Customer
+        fields = ['first_name', 'last_name', 'account']
+
+    def create(self, validated_data):
+        account_data = validated_data.pop('account')
+        account_data['user_type'] = 'customer'
+        print(account_data)
+        account = AccountRegistrationSerializer(data=account_data)
+        account.is_valid()
+        account = account.save()
+        customer = Customer.objects.create(**validated_data, account=account)
+        return customer
+
+
 class VendorRegisterSerializer(serializers.ModelSerializer):
-  account = AccountRegistrationSerializer()
-  class Meta:
-    model = Vendor
-    fields = ['company_name', 'company_address', 'account']
+    account = AccountRegistrationSerializer()
 
-  def create(self, validated_data):
-    account_data = validated_data.pop('account')
-    account_data['user_type'] = 'vendor'
-    print(account_data)
-    account = AccountRegistrationSerializer(data=account_data)
-    account.is_valid()
-    account = account.save()
-    customer = Vendor.objects.create(**validated_data, account=account)
-    return customer
+    class Meta:
+        model = Vendor
+        fields = ['company_name', 'company_address', 'account']
+
+    def create(self, validated_data):
+        account_data = validated_data.pop('account')
+        account_data['user_type'] = 'vendor'
+        print(account_data)
+        account = AccountRegistrationSerializer(data=account_data)
+        account.is_valid()
+        account = account.save()
+        customer = Vendor.objects.create(**validated_data, account=account)
+        return customer
 
 
 class AccountSerializer(serializers.ModelSerializer):
-    profile_image = serializers.SerializerMethodField('get_image_url')
+    profile_image = serializers.SerializerMethodField()
+
     class Meta:
         model = Account
-        fields = ['id', 'email', 'phone', 'state', 'city','user_type', 'profile_image']
+        fields = ['id', 'email', 'phone', 'state',
+                  'city', 'user_type', 'profile_image']
 
         extra_kwargs = {
-          'user_type': {'required': True},
+            'user_type': {'required': True},
         }
 
-    def get_image_url(self, obj):
+    def get_profile_image(self, obj):
         request = self.context.get("request")
         print('this method is called')
         return obj.profile_image.url
